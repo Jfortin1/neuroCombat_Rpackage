fa9540f5c2a12e720ba2ce61d20a680fa8de8cff
Jean-Philippe Fortin <fortin.jeanphilippe@gene.com>
2021-06-03 08:30:49 -0700

    Move directory

---
+++ R/imports.R
@@ -0,0 +1,5 @@
+#' @importFrom matrixStats rowVars rowSds
+#' @importFrom matrixStats rowMeans2 rowSums2
+#' @importFrom stats rnorm rgamma var density model.matrix median
+#' @importFrom graphics lines plot
+NULL
